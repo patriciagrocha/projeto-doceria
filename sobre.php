21bfe4e06e858c845ebc0bcfe45e1bd8db7fcd2d
Alanvictorbs <144290304+Alanvictorbs@users.noreply.github.com>
2023-11-17 09:41:29 -0300

    protótipo da págia sobre com texto do figma.

--- sobre.php
+++ sobre.php
@@ -3,56 +3,18 @@
 <head>
     <style>
         /* Estilos para a página */
-        body {
-            font-family: "Roboto";
-            margin: 0;
-            padding: 0;
-        }
-
-        .container {
-            font-family:arial, sans-serif;
-            font-size: 20px;
-        }
-
-
-        .h1 {
-            color: rgb(96, 25, 25);
-            font-family: "Roboto";
-            text-align: center;
-            font-size: 40px;
-            padding-bottom: 20px;
-            border-bottom: solid white;
-
-        }
-
-        .p {
-            font-size: 25px;
-        }
+      body{
+         background-image: url("image 3.png")
+      }
+      #toposobre{
+        width: 100%;
+        margin-top: -15px;
+      }
     </style>
 </head>
-<div class= "body">
 <body>
-<div class="container">
-        <h1 div class= "h1">Como Realizar o meu Pedido?</h1> </div>
-
-        <ol><div class="p">
-            <strong><li>Solicite um Orçamento:</li></strong>
-            <p>Entre em contato conosco através do nosso número de telefone fornecido em nossos catálogos e rodapé do site. Certifique-se de incluir detalhes como a data do evento, tipo de sobremesa desejada e número de convidados para que possamos melhor lhe atender.</p>
-
-            <strong><li>Agendamento:</li></strong>
-            <p>Os orçamentos de pedidos de Bolos Perosnalizados só serão fechados se tiverem, no mínimo, <strong>4 dias de antecedência</strong> e pedidos de Doces Gourmet com, no mínimo, <strong>3 dias de antecedência</strong> para garantir que possamos atendê-lo com a qualidade e pontualidade que você merece.</p>
-
-
-            <strong><li>Confirmação do Pedido:</li></strong>
-            <p>Após recebermos seu pedido, entraremos em contato para confirmar os detalhes, discutir opções de personalização e fornecer um orçamento final. Certifique-se de revisar e concordar com os detalhes antes de confirmar o pedido.</p>
-
-            <strong><li>Transporte:</li></strong>
-            <p>O trasnporte é de total responsabilidade do cliente, sendo recomendaddo a retirada no local. Também aceitamos retirada por aplicativo de carona/corrida na modalidade CARRO, a qual também é de repsonsabilidade do cliente. Se o veículo for de duas rodas, não garantimos a integridade do prodouto.</p>
-
-            <strong><li>Desfrute das Delícias:</li></strong>
-            <p>Ao receber suas sobremesas, desfrute de cada mordida e compartilhe com seus amigos e familiares. Esperamos que você tenha uma experiência doce e memorável!</p>
-            </div>
-        </ol>
-    </body>
-    </div>
-    </html>
+  <img id="toposobre" src="plano-de-fundo-com-ingredientes-para-cozinhar-assar-planta-plana-vista-superior_266870-9722 1.png">
+  <p class="text1">Meu nome é Graziela, e sempre tive uma paixão secreta pela confeitaria. Adorava fazer bolos deliciosos para minha família e amigos em ocasiões especiais. Meus bolos eram famosos entre meus amigos e familiares, e eles sempre me diziam que eu deveria abrir minha própria confeitaria.</p><br>
+  <p class="text2">Comecei devagar, fazendo bolos para aniversários de amigos e eventos locais. Com o tempo, minha clientela cresceu à medida que as pessoas provavam meus bolos incríveis e compartilhavam a notícia. Investi em um pequeno espaço de cozinha comercial e comecei a aceitar encomendas online.</p>
+</body>
+</html>
