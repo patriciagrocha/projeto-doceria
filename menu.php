fa62a0453ecc401d6e64a2e6670321aa6c443c9e
Graziela Lira <grazielalira@gmail.com>
2023-11-20 22:21:47 -0300

    alteração cor dos link do menu

--- menu.php
+++ menu.php
@@ -1,74 +1,4 @@
 <div>
-<<<<<<< HEAD
-<head>
-  <style>
-/* Estilos do menu */
-.menu {
-  height: 100px;
-  list-style-type: none;
-  margin-top: 0;
-  overflow: hidden;
-  background-color: #601616;
-}
-
-.menu li {
-  float: right;
-  margin-top: 25px;
-  text-align: center;
-}
-
-.menu li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  margin-right: 50px;
-}
-
-.menu li a:hover:not(.active) {
-  background-color: #111;
-  border-radius: 50px;
-}
-
-.menu .active {
-  background-color: #601616;
-}
-
-.menu .logo {
-  width: 300px;
-  height: 100px;
-  overflow: hidden;
-  margin-top: 0;
-  margin-left: 0;
-}
-
-/* Conteúdos específicos dos links do menu */
-#inicio {
-  background-color: #420606;
-  border-top-left-radius: 50px;
-  border-bottom-left-radius: 50px;
-}
-
-#sobre {
-  background-color: #420606;
-}
-
-#catalogo {
-  background-color: #420606;
-}
-
-#avaliacoes {
-  background-color: #420606;
-  border-top-right-radius: 50px;
-  border-bottom-right-radius: 50px;
-}
-
-  /*fim dos conteudos dos links do menu*/
-  </style>
-</head>
-<div id="menu">
-=======
   <head>
     <style>
       .menu {
@@ -101,41 +31,36 @@
       }
 
       li a:hover {
-        background-color: #111;
+        background-color: #420606;
       }
       
       .inicio {
-        background-color: #420606;
+        background-color: #601616;
         
       }
 
       /* Conteúdos dos links do menu */
       #sobre {
-        background-color: #420606;
+        background-color: #601616;
       }
 
       #catalogo {
-        background-color: #420606;
+        background-color: #601616;
       }
 
       #avaliacoes {
-        background-color: #420606;
+        background-color: #601616;
        
       }
       /* Fim dos conteúdos dos links do menu */
     </style>
   </head>
   <div id="menu">
->>>>>>> 4be0bf237c7cfce55e07b554e924337228b9364e
     <ul class="menu">
       <li><a class="inicio" href="index.php">Inicio</a></li>
       <li id="catalogo"><a href="?pg=catalogos">Catálogo</a></li>
       <li id="sobre"><a href="?pg=sobre">Sobre mim</a></li>
       <li id="avaliacoes"><a href="?pg=avaliacoes">Fale conosco</a></li>
     </ul>
-<<<<<<< HEAD
-</div>
-=======
   </div>
->>>>>>> 4be0bf237c7cfce55e07b554e924337228b9364e
 </div>
